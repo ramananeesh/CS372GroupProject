be2a75c6b0f7873e7f420cf76a11b8b25912c5e6
Matthew Hunt <huntmj01@students.ipfw.edu>
2017-11-26 23:25:57 +0000

    renames style.css to html-builder.css and moves it from base directory to css folder

--- html-builder.php
+++ html-builder.php
@@ -7,7 +7,7 @@ function insertHeader($title = NULL, $descrip = NULL){
     echo '<meta charset="utf-8">';
     echo '<link rel="tempbox_icon" href="./images/logo.ico" />';
     echo '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
-    echo '<link href="style.css" rel="stylesheet" type="text/css">';
+    echo '<link href="./css/html-builder.css" rel="stylesheet" type="text/css">';
     
     if($title != NULL){
         echo "<title>$title</title>";
