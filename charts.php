c3f58c698b5d09a805e61267dfd61a77331d8cb6
Aneesh Raman <ramaa02@students.ipfw.edu>
2017-11-30 19:24:16 +0000

    charts.php uses actual data from DB

--- charts.php
+++ charts.php
@@ -1,3 +1,39 @@
+<?php
+    $arrD=array(
+    0  => 1,
+    1 => 2,
+    2 => 3,
+    3 => 4,
+    4 => 5,
+    5 =>6,
+    6 =>7);
+  
+    
+    require('database.php');
+    $connection=dbConnect();
+    
+    $sql="select count(id) from users where dateActive>=CURRENT_DATE-7; ";
+    
+    $arrU=array();
+    for($i=0;$i<7;$i++){
+        $sql="select count(id) from users where dateActive=CURRENT_DATE-($i+1); ";
+        if($result=mysqli_query($connection,$sql)){
+            $row=mysqli_fetch_assoc($result);
+            $x=intVal($row['count(id)']);
+            $arrU[]=$x;
+        }
+    }
+    
+    $arrF=array();
+    for($i=0;$i<7;$i++){
+        $sql="select count(id) from files where upload_date=CURRENT_DATE-($i+1); ";
+        if($result=mysqli_query($connection,$sql)){
+            $row=mysqli_fetch_assoc($result);
+            $x=intVal($row['count(id)']);
+            $arrF[]=$x;
+        }
+    }
+?>
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -6,22 +42,18 @@
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-        var arrDay=[1,2,3,4,5,6,7];
-        var arrUsers=[10,2,4,8,12,6,5];
-        var arrFiles=[8,2,4,7,6,12,14];
+        //var arrDay=[1,2,3,4,5,6,7];
+        var arrDay=JSON.parse('<?php echo json_encode($arrD); ?>');
+        //var arrUsers=[10,2,4,8,12,6,5];
+        var arrUsers=JSON.parse('<?php echo json_encode($arrU); ?>');
+        //var arrFiles=[8,2,4,7,6,12,14];
+        var arrFiles=JSON.parse('<?php echo json_encode($arrF); ?>');
         var arr=[];
         arr[0]=['Day','Users','Files'];
         for(var i=0;i<7;i++){
             arr[i+1]=[arrDay[i],arrUsers[i],arrFiles[i]];
         }
-        alert("Hi " +arr[0]);
-        /*var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2004',  1000,      400],
-          ['2005',  1170,      460],
-          ['2006',  660,       1120],
-          ['2007',  1030,      540]
-        ]);*/
+        
         var data=google.visualization.arrayToDataTable(arr);
         var options = {
           title: 'Website Usage',
