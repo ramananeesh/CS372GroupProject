ccfe593662d7d211a94a182039beaa2a3a99c1f0
Joel Stauffer <staujd02@students.ipfw.edu>
2017-12-06 01:58:22 +0000

    The very last change!

--- verify.php
+++ verify.php
@@ -8,8 +8,34 @@
     <head>
         <title>Verifying User</title>
         <script src="./js/jquery-1.11.0.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/sweetalert2/4.2.4/sweetalert2.min.css" rel="stylesheet"/>
+        <script src="https://cdn.jsdelivr.net/sweetalert2/4.2.4/sweetalert2.min.js"></script>
         <script type="text/javascript" src="./js/admin.js"></script>
         <script type="text/javascript">
+        
+        function makeAjaxRequest(type, url, data, func){
+    $.ajax({
+        type: type,                                           // GET or POST
+        url: url,                                               // Path to file
+        data: data,
+        timeout: 10000,                                          // Waiting time
+        beforeSend: function() {                                // Before Ajax 
+          //$content.append('<div id="load">Loading</div>');      // Load message
+        },
+        complete: function() {                                  // Once finished
+          //$('#load').remove();                               // Clear message
+        },
+        success: function(data){
+            //swal.close();
+            func(data);
+        },
+        fail: function() {                                      // Show error msg 
+            alert("Error with Ajax call!");
+          //$('#panel').html('<div class="loading">Please try again soon.</div>');
+        }
+    });
+}
         
         function init(){
             if(sessionStorage.getItem("userName") === null ||
@@ -37,8 +63,12 @@
         
         init();
         </script>
+        
     </head>
+    
     <body>
-        <h1>Verifying... You will be redirected shortly...</h1>
+        <h1>Verifying... You will be redirected shortly...</h1
+        
     </body>
+    
 </html>
